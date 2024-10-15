0ed6deeddc31cbf7c640a34845aa85f488150933
PtoZorro <javiersanchezferri12@gmail.com>
2024-10-15 22:33:04 +0200

    fijado de enemigos

--- SoulsLike_Proto/Assets/SoulsLike-Proto/Scripts/Player-Scripts/CameraFollow.cs
+++ SoulsLike_Proto/Assets/SoulsLike-Proto/Scripts/Player-Scripts/CameraFollow.cs
@@ -1,34 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+using UnityEngine.Animations;
 
 public class CameraFollow : MonoBehaviour
 {
     [Header("External References")]
-    //[SerializeField] GameObject Camera;
     [SerializeField] Transform target;
     [SerializeField] PlayerInput playerInput;
+    [SerializeField] GameObject LockPointSprite;
+    [SerializeField] LayerMask enemyLayer;
+
+    [Header("Private References")]
+    GameObject enemyLocked;
 
     [Header("Stats")]
     [SerializeField] float cameraMoveSpeed;
     [SerializeField] float clampAngle;
     [SerializeField] float mouseSensitivity;
     [SerializeField] float stickSensitivity;
+    [SerializeField] float lockEnemyRadius;
     private float rotX;
     private float rotY;
 
     [Header("Conditional Values")]
     bool isGamepad;
+    bool camLocked;
 
+    [Header("Input")]
     Vector2 lookInput;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Definir valores de inicio
         Vector3 rot = transform.localRotation.eulerAngles;
         rotX = rot.x;
         rotY = rot.y;
+        enemyLocked = null;
+        LockPointSprite.SetActive(false);
 
         // Bloquar y esconder cursor
         Cursor.lockState = CursorLockMode.Locked;
@@ -39,7 +52,9 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         // Manejo de rotación de cámara
-        HandleRotation();
+        if (!camLocked) HandleRotation();
+        // Fijación de cámara hacia un enemigo
+        else if (camLocked) LookAtEnemy();
     }
 
     void LateUpdate()
@@ -68,6 +83,55 @@ public class CameraFollow : MonoBehaviour
         transform.rotation = localRotation;
     }
 
+    void LockDirection()
+    {
+        // Si la cámara no está fijada en el enemigo, fijamos
+        if (!camLocked)
+        {
+            // Comprobamos todos los enemigos cercanos
+            Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, lockEnemyRadius, enemyLayer);
+
+            // Si hemos detectado enemigos procedemos a fijar
+            if (enemiesInRange != null && enemiesInRange.Length > 0)
+            {
+                //LockPointSprite.SetActive(true);
+
+                // Definimos la distancia mas cercana para saber que enemigo estará más cerca
+                float closestDistance = lockEnemyRadius;
+
+                // Comprobamos uno a uno la distancia de todos los enemigos
+                foreach (Collider enemyCollider in enemiesInRange)
+                {
+                    // Calculamos distancia del enemigo 
+                    float distance = Vector3.Distance(transform.position, enemyCollider.gameObject.transform.position);
+
+                    // Cuando obtenemos el enemigo con la menor distancia lo seleccionamos y almacenamos
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        enemyLocked = enemyCollider.gameObject;
+                    }
+                }
+
+                // Indicamos que la cámara está fijada
+                camLocked = true;
+            }
+        }
+        else if (camLocked) 
+        {
+            //LockPointSprite.SetActive(false);
+            
+            // Indicamos que la cámara ya no debe estar fijada
+            camLocked = false; 
+        }
+    }
+
+    void LookAtEnemy()
+    {
+        // La cámara apunta siempre al enemigo
+        transform.LookAt(enemyLocked.transform.position);
+    }
+
     void FollowPlayer()
     {
         // Seguimiento de la camara al jugador con la posición del target y una velocidad asignada
@@ -79,6 +143,14 @@ public class CameraFollow : MonoBehaviour
     public void OnLook(InputAction.CallbackContext context)
     {
         lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLockCam(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            LockDirection();
+        }
     }
 
     public void OnDeviceChange()
